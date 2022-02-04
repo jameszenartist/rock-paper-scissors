e140b0ebab490e21515fe26cc90eef0223226976
James Hansen <jameszenartist@gmail.com>
2022-02-03 20:43:30 -0500

    made js changes

--- src/Scoreboard.js
+++ src/Scoreboard.js
@@ -1,9 +1,9 @@
-function Scoreboard({ count, compCount }) {
+function Scoreboard({ count, countRef, compCountRef, compCount, useRef }) {
   return (
     <div className="scoreboard">
       <h2>SCOREBOARD</h2>
-      <h3>PLAYER: {count}</h3>
-      <h3>COMPUTER: {compCount}</h3>
+      <h3>PLAYER: {countRef.current}</h3>
+      <h3>COMPUTER: {compCountRef.current}</h3>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
 import Scoreboard from "./Scoreboard";
 import Rock from "./Rock";
@@ -10,24 +10,20 @@ import "./index.css";
 function App() {
   const [num, setNum] = useState(null);
   const [text, setText] = useState("CHOOSE WISELY");
-  const [count, setCount] = useState(0);
-  const [compCount, setCompCount] = useState(0);
+  // const [count, setCount] = useState(0);
+  // const [compCount, setCompCount] = useState(0);
+  const countRef = useRef(0);
+  const compCountRef = useRef(0);
 
-  function changeScore(str, num1, num2) {
-    if (str === "YOU WIN!") {
-      setCount(num1 + 1);
+  useEffect(() => {
+    if (text === "YOU WIN!") {
+      countRef.current += 1;
+      console.log(countRef.current);
+    } else if (text === "YOU LOSE!") {
+      compCountRef.current += 1;
+      console.log(compCountRef.current);
     }
-    if (str === "YOU LOSE!") {
-      setCompCount(num2 + 1);
-    }
-  }
-
-  useEffect(
-    (text, count, compCount) => {
-      changeScore(text, count, compCount);
-    },
-    [text]
-  );
+  }, [text]);
 
   const handleClick = (num) => {
     setText("CHOOSE WISELY");
@@ -37,7 +33,13 @@ function App() {
 
   return (
     <div className="container">
-      <Scoreboard count={count} compCount={compCount} />
+      <Scoreboard
+        text={text}
+        countRef={countRef}
+        compCountRef={compCountRef}
+        // count={count}
+        // compCount={compCount}
+      />
       <div className="status">
         <h1>{text}</h1>
         <h1 className="pulse">
